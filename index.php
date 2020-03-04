effa044129b22b2138b4bab6505b63b54e28d386
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-03-03 22:34:39 -0700

    Getting data from database and clean memory

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     if(!$conn) {
         echo 'Connection error: ' . mysqli_connect_error();
     }
+
+    // write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    // make query and get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
