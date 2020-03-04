9d711e26bd3761072c54a1b545f768ab5ee8f3be
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-03-03 22:25:18 -0700

    Connect to a database

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+    // connect to database
+    $conn = mysqli_connect('localhost', 'admin2', 'test1234', 'pizza_builder');
+
+    // check connection
+    if(!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 ?>
 
 <!DOCTYPE html>
