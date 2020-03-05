9bf60df24ff0fbd5fbe57719c58ef170060b1306
Dmitry Andrianov <dima.andrianov@gmail.com>
2020-03-04 18:55:14 -0700

    Delete a record from database.

--- details.php
+++ details.php
@@ -2,6 +2,19 @@
 
     include('config/db_connect.php');
 
+    if(isset($_POST['delete'])) {
+
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+        $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+        if(mysqli_query($conn, $sql)) {
+            header('Location: index.php');
+        } else {
+            echo 'query error : ' . mysqli_error($conn);
+        }
+    }
+
     // check GET request id param
     if(isset($_GET['id'])) {
 
@@ -20,7 +33,6 @@
         mysqli_close($conn);
 
     }
-
 ?>
 
 <!DOCTYPE html>
@@ -28,18 +40,25 @@
 
     <?php include('templates/header.php'); ?>
 
-        <div class="container center">
-            <?php if($pizza): ?>
+    <div class="container center">
+		<?php if($pizza): ?>
+			<h4><?php echo $pizza['title']; ?></h4>
+			<p>Created by <?php echo $pizza['email']; ?></p>
+			<p><?php echo date($pizza['created_at']); ?></p>
+			<h5>Ingredients:</h5>
+			<p><?php echo $pizza['ingredients']; ?></p>
 
-            <h4> <?php echo htmlspecialchars($pizza['title']); ?> </h4>
-            <p> Created by: <?php htmlspecialchars($pizza['email']); ?> </p>
-            <p> <?php echo date($pizza['created_at']) ?> </p>
-            <h5> Ingredients </h5>
-            <p> <?php echo htmlspecialchars($pizza['ingredients']); ?> </p>
-            <?php else: ?>
-            <h5> No such pizza exists! </h5>
-            <?php endif; ?>
-        </div>
+			<!-- DELETE FORM -->
+			<form action="details.php" method="POST">
+				<input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+				<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+			</form>
+
+		<?php else: ?>
+			<h5>No such pizza exists.</h5>
+		<?php endif ?>
+	</div>
+
 
     <?php include('templates/footer.php'); ?>
     
